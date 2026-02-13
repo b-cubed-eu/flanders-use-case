5c69e43f6a6b888bc57651b92cbd972d36c0ef3f
jrhillae <jasmijn.hillaert@gmail.com>
2026-02-13 11:26:47 +0100

    phylogenetic diversity
    #24

--- _preprocessing/R/pdIndicator/create_pd_indicator.R
+++ _preprocessing/R/pdIndicator/create_pd_indicator.R
@@ -11,4 +11,4 @@ geojson_Vl <- read_sf("~/GitHub/Flanders_use_case/_preprocessing/data/raw/spatia
 pd_Vl <- geojson_Vl %>%
   left_join(pd_cube_final, by = c("CELLCODE" = "eeacellcode"))
 
-write_sf(pd_Vl, "~/GitHub/Flanders_use_case/_preprocessing/data/output/pdindicator/pd_Vl.geojson")
+write_sf(pd_Vl, "~/GitHub/Flanders_use_case/src/pd_Vl.geojson")
