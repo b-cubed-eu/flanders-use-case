1d7434a378e394ddb34795f39076e6a87e2d71ae
jrhillae <jasmijn.hillaert@gmail.com>
2026-04-30 16:54:14 +0200

    add phylogenetic indicator to dashboard
    #24

--- _preprocessing/R/pdIndicator/create_pd_indicator.R
+++ _preprocessing/R/pdIndicator/create_pd_indicator.R
@@ -1,14 +1,33 @@
-#waarden pd-indicator
-pd_cube_final <- read.csv("~/GitHub/Flanders_use_case/_preprocessing/data/interim/pd_cube_final.csv")
-
 #waarden geometry
 library(sf)
 library(dplyr)
 
-geojson_Vl <- read_sf("~/GitHub/Flanders_use_case/_preprocessing/data/raw/spatial/EEA_1km_Flanders_union.geojson")
+#waarden pd-indicator
+pd_cube_Fl_1km <- read.csv("./_preprocessing/data/interim/pd_indicator/pd_cube_flanders_1km.csv")
 
+pd_cube_Fl_5km <- read.csv("./_preprocessing/data/interim/pd_indicator/pd_cube_flanders_5km.csv")
 
-pd_Vl <- geojson_Vl %>%
-  left_join(pd_cube_final, by = c("CELLCODE" = "eeacellcode"))
+pd_loss_cube_Fl_1km <- read.csv("./_preprocessing/data/interim/pd_indicator/pd_loss_cube_1km.csv")
 
-write_sf(pd_Vl, "~/GitHub/Flanders_use_case/src/pd_Vl.geojson")
+pd_loss_cube_Fl_5km <- read.csv("./_preprocessing/data/interim/pd_indicator/pd_loss_cube_5km.csv")
+
+geojson_Vl_1km <- read_sf("./_preprocessing/data/raw/spatial/EEA_1km_Flanders_union.geojson")
+
+geojson_Vl_5km <- read_sf("./_preprocessing/data/raw/spatial/EEA_5km_Flanders_union.geojson")
+
+pd_Vl_1km <- geojson_Vl_1km %>%
+  left_join(pd_cube_Fl_1km, by = c("CELLCODE" = "eeacellcode"))
+
+pd_Vl_5km <- geojson_Vl_5km %>%
+  left_join(pd_cube_Fl_5km, by = c("cellCode" = "eeacellcode"))
+
+pd_loss_1km <- geojson_Vl_1km %>%
+  left_join(pd_loss_cube_Fl_1km, by = c("CELLCODE" = "eeacellcode"))
+
+pd_loss_5km <- geojson_Vl_5km %>%
+  left_join(pd_loss_cube_Fl_5km, by = c("cellCode" = "eeacellcode"))
+
+write_sf(pd_Vl_1km, "~/GitHub/Flanders_use_case/src/pdIndicator/pd_Vl_1km.geojson")
+write_sf(pd_Vl_5km, "~/GitHub/Flanders_use_case/src/pdIndicator/pd_Vl_5km.geojson")
+write_sf(pd_loss_1km, "~/GitHub/Flanders_use_case/src/pdIndicator/pd_loss_Vl_1km.geojson")
+write_sf(pd_loss_5km, "~/GitHub/Flanders_use_case/src/pdIndicator/pd_loss_Vl_5km.geojson")
